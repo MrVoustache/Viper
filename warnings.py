1bbfd46fd78b8e8821b03b6af89c9ea0205c25a4
Vincent Raulin <vincent.raulin.n7@gmail.com>
2022-09-06 12:12:14 +0200

    Added the more general base exception "SecurityWarning"

--- warnings.py
+++ warnings.py
@@ -4,13 +4,23 @@ This module adds a few more useful warning classes to Python.
 
 from warnings import simplefilter
 
-__all__ = ["VulnerabilityWarning"]
+__all__ = ["SecurityWarning", "VulnerabilityWarning"]
 
 
 
 
 
-class VulnerabilityWarning(Warning):
+class SecurityWarning(Warning):
+
+    """
+    This class indicates that some bare security requirements could not be met.
+    """
+
+simplefilter("default", SecurityWarning)
+
+
+
+class VulnerabilityWarning(SecurityWarning):
 
     """
     This class indicates that a vulnerability just appeared in the executing code and could be (or has been) used by an attacker.
