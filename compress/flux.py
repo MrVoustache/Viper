44dbd2c99a8ef63c42d964848b6109e02585c044
Vincent Raulin <vincent.raulin.n7@gmail.com>
2022-09-07 13:36:48 +0200

    Snappy compression will only be available if the snappy package is available

--- compress/flux.py
+++ compress/flux.py
@@ -5,7 +5,7 @@ This module defines FluxOperators for multiple compression and decompression alg
 from Viper.abc.flux import FluxOperator
 from Viper.abc.io import BytesReader, BytesWriter
 
-__all__ = ["BZ2CompressorOperator", "BZ2DecompressorOperator", "LZMACompressorOperator", "LZMADecompressorOperator", "SnappyCompressorOperator", "SnappyDecompressorOperator"]
+__all__ = ["BZ2CompressorOperator", "BZ2DecompressorOperator", "LZMACompressorOperator", "LZMADecompressorOperator"]
 
 PACKET_SIZE = 2 ** 16
 
@@ -165,83 +165,95 @@ LZMADecompressorOperator.inverse = LZMACompressorOperator
 
 
 
-class SnappyCompressorOperator(FluxOperator):
+try:
+    from snappy import StreamCompressor
+    del StreamCompressor
+    ok = True
+except:
+    ok = False
 
-    """
-    Performs the compression of input stream into output stream with Google's snappy algorithm.
-    """
+if ok:
 
-    def __init__(self, source: BytesReader, destination: BytesWriter, *, auto_close: bool = False) -> None:
-        super().__init__(source, destination, auto_close=auto_close)
-        self.__done = False
+    class SnappyCompressorOperator(FluxOperator):
 
-    def run(self):
-        from snappy import StreamCompressor
-        from Viper.abc.io import IOClosedError
-        compressor = StreamCompressor()         ### IS THE BASIC compress() FUNCTION FASTER?
-        while True:
+        """
+        Performs the compression of input stream into output stream with Google's snappy algorithm.
+        """
+
+        def __init__(self, source: BytesReader, destination: BytesWriter, *, auto_close: bool = False) -> None:
+            super().__init__(source, destination, auto_close=auto_close)
+            self.__done = False
+
+        def run(self):
+            from snappy import StreamCompressor
+            from Viper.abc.io import IOClosedError
+            compressor = StreamCompressor()         ### IS THE BASIC compress() FUNCTION FASTER?
+            while True:
+                try:
+                    packet = self.source.read(PACKET_SIZE)
+                except IOClosedError:
+                    break
+                try:
+                    self.destination.write(compressor.compress(packet))
+                except IOClosedError as e:
+                    raise RuntimeError("The destination stream got closed before the operator could finish writing its output") from e
             try:
-                packet = self.source.read(PACKET_SIZE)
-            except IOClosedError:
-                break
-            try:
-                self.destination.write(compressor.compress(packet))
+                self.destination.write(compressor.flush() or b"")
             except IOClosedError as e:
                 raise RuntimeError("The destination stream got closed before the operator could finish writing its output") from e
-        try:
-            self.destination.write(compressor.flush() or b"")
-        except IOClosedError as e:
-            raise RuntimeError("The destination stream got closed before the operator could finish writing its output") from e
-        self.__done = True
-        if self.auto_close:
-            self.destination.close()
-    
-    @property
-    def finished(self) -> bool:
-        return self.__done
+            self.__done = True
+            if self.auto_close:
+                self.destination.close()
+        
+        @property
+        def finished(self) -> bool:
+            return self.__done
 
 
 
 
-class SnappyDecompressorOperator(FluxOperator):
+    class SnappyDecompressorOperator(FluxOperator):
 
-    """
-    Performs the decompression of input stream into output stream with Google's snappy algorithm.
-    """
+        """
+        Performs the decompression of input stream into output stream with Google's snappy algorithm.
+        """
 
-    def __init__(self, source: BytesReader, destination: BytesWriter, *, auto_close: bool = False) -> None:
-        super().__init__(source, destination, auto_close=auto_close)
-        self.__done = False
+        def __init__(self, source: BytesReader, destination: BytesWriter, *, auto_close: bool = False) -> None:
+            super().__init__(source, destination, auto_close=auto_close)
+            self.__done = False
 
-    def run(self):
-        from snappy import StreamDecompressor
-        from Viper.abc.io import IOClosedError
-        decompressor = StreamDecompressor()
-        while True:
+        def run(self):
+            from snappy import StreamDecompressor
+            from Viper.abc.io import IOClosedError
+            decompressor = StreamDecompressor()
+            while True:
+                try:
+                    packet = self.source.read(PACKET_SIZE)
+                except IOClosedError:
+                    break
+                try:
+                    self.destination.write(decompressor.decompress(packet))
+                except IOClosedError as e:
+                    raise RuntimeError("The destination stream got closed before the operator could finish writing its output") from e
             try:
-                packet = self.source.read(PACKET_SIZE)
-            except IOClosedError:
-                break
-            try:
-                self.destination.write(decompressor.decompress(packet))
+                self.destination.write(decompressor.flush())
             except IOClosedError as e:
                 raise RuntimeError("The destination stream got closed before the operator could finish writing its output") from e
-        try:
-            self.destination.write(decompressor.flush())
-        except IOClosedError as e:
-            raise RuntimeError("The destination stream got closed before the operator could finish writing its output") from e
-        self.__done = True
-        if self.auto_close:
-            self.destination.close()
-    
-    @property
-    def finished(self) -> bool:
-        return self.__done
+            self.__done = True
+            if self.auto_close:
+                self.destination.close()
+        
+        @property
+        def finished(self) -> bool:
+            return self.__done
 
 
-SnappyCompressorOperator.inverse = SnappyDecompressorOperator
-SnappyDecompressorOperator.inverse = SnappyCompressorOperator
+    SnappyCompressorOperator.inverse = SnappyDecompressorOperator
+    SnappyDecompressorOperator.inverse = SnappyCompressorOperator
 
+    __all__.extend(("SnappyCompressorOperator", "SnappyDecompressorOperator"))
+
+del ok
 
 
 del FluxOperator, BytesReader, BytesWriter
