1d550c41139bffae68a197c93a1526f72be91297
MrVoustache <vincent.raulin.n7@gmail.com>
2022-09-14 21:24:53 +0200

    FallenThreads now have a finalizing priority. All FallenThreads with the highest priority will be shutdown as a batch, then those with the new highest priority and so on.

--- better_threading.py
+++ better_threading.py
@@ -8,7 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 from threading import Event, RLock, Thread
 from typing import Any, Callable, Generic, Iterable, Mapping, Set, TypeVar
 
-__all__ = ["Future", "DaemonThread", "FallenThread", "DeamonPoolExecutor"]
+__all__ = ["Future", "DaemonThread", "FallPriority", "FallenThread", "DeamonPoolExecutor"]
 
 
 
@@ -102,6 +102,19 @@ class DaemonThread(Thread):
 
 
 
+
+class FallPriority:
+
+    """
+    Just a placeholder for priorities
+    """
+
+    EXTREME = 100
+    HIGH = 80
+    MEDIUM = 60
+    LOW = 40
+    VERY_LOW = 20
+
 class FallenThread(DaemonThread):
 
     """
@@ -110,21 +123,21 @@ class FallenThread(DaemonThread):
     The FallenThread will be judged disposable when its finalization completes.
     """
 
-    _activation_lock = RLock()
-    _new_fallen = Event()
 
-    def __init__(self, finalizing_callback : Callable[[], None], group: None = None, target: Callable[..., Any] | None = None, name: str | None = None, args: Iterable[Any] = (), kwargs: Mapping[str, Any] | None = None) -> None:
-        with FallenThread._activation_lock:
-            super().__init__(group, target, name, args, kwargs)
+    def __init__(self, finalizing_callback : Callable[[], None], group: None = None, target: Callable[..., Any] | None = None, name: str | None = None, args: Iterable[Any] = (), kwargs: Mapping[str, Any] | None = None, priority : int | float = FallPriority.MEDIUM) -> None:
+        
+        if not isinstance(priority, int | float):
+            raise TypeError("Expected int or float, got " + repr(type(priority).__name__))
+        if priority < 0 or priority == float("nan"):
+            raise ValueError("Expected positive number for priority, got " + repr(priority))
 
-            from threading import Event
-
-            if not callable(finalizing_callback):
+        if not callable(finalizing_callback):
                 raise TypeError("Expected callable for finalizing_callback, got " + repr(finalizing_callback.__class__.__name__))
-            
-            self._finalizing_callback = finalizing_callback
-            self._finalizing_event = Event()        # To tell when finalization has started
-            FallenThread._new_fallen.set()          # To tell that a new FallenThread has been created
+        
+        super().__init__(group, target, name, args, kwargs)
+
+        self.__priority = priority
+        self.__finalizing_callback = finalizing_callback
 
 
 
@@ -134,97 +147,41 @@ class FallenThread(DaemonThread):
 @atexit.register
 def save_fallen_threads():
     """
-    This function is responsible for calling the finalization process. It will:
+    This function is responsible for calling the finalization process. For each priority level (descending), it will:
         - Start a new DeamonThread for each finalization callback
         - Wait for the completion of all finalizations
-        - Wait until both tasks are finished
     """
 
-    from threading import enumerate, RLock, Event
+    from threading import enumerate
 
-    def _finalize(t : FallenThread):
-        """
-        Just a little handler for notifying ans starting the finalizer
-        """
-        t._finalizing_event.set()
+    finalized = set()
+
+    def finalize(t : FallenThread):
         try:
-            t._finalizing_callback()
-        except:
-            print("Exception in FallenThread {}'s finalizer :".format(t.name))
-            raise
+            t._FallenThread__finalizing_callback()
+        finally:
+            finalized.add(t)
 
-    alive_finalizers : Set[DaemonThread] = set()    # Running finalizer DeamonThreads
-    finalization_lock = RLock()                     # Ensure proper transmission of work between the two tasks
-    new_finalizing = Event()                        # A new FallenThread has started finalizing
-    finished = [False, False]                       # Indicates if both tasks have finished
-    checker_lock = RLock()                          # Ensures the correctness when checking for the completion of tasks
-    checker_event = Event()                         # Notifies for a checking round
+    while True:
 
-    def _start_finalizers():
-        """
-        This function will stay alive until the end and wait start the finalizers of all FallenThreads
-        """
-        while True:
-            if not FallenThread._new_fallen.is_set():   # No new fallen -> no work
-                with checker_lock:
-                    finished[0] = True
-                    checker_event.set()
-            FallenThread._new_fallen.wait()     # New fallen!
-            with checker_lock:
-                finished[0] = False
+        remaining_fallen = {t for t in enumerate() if isinstance(t, FallenThread) and t not in finalized}
+        if not remaining_fallen:
+            break
+        current_level = max(t._FallenThread__priority for t in remaining_fallen)
+        batch = {t for t in remaining_fallen if t._FallenThread__priority == current_level}
 
-            with FallenThread._activation_lock:     # Handling them
-                FallenThread._new_fallen.clear()
-                alive_threads = {t for t in enumerate() if isinstance(t, FallenThread) and t.is_alive()}
-                for t in alive_threads:
+        active_finalizers : set[DaemonThread] = set()
 
-                    if not t._finalizing_event.is_set():    # I should start its finalizer
-                        d = DaemonThread(target = _finalize, args = (t, ))
-                        d.start()
-                        t._finalizing_event.wait()
-                        alive_finalizers.add(d)
-                        with checker_lock:
-                            finished[1] = False     # The other task has some work to do now
-                        with finalization_lock:
-                            new_finalizing.set()    # Notifying the other task
+        for t in batch:
+            
+            d = DaemonThread(target = finalize, args = (t, ), name = "Finalizer of thread " + repr(t.name))
+            active_finalizers.add(d)
+            d.start()
+        
+        for d in active_finalizers:
+            d.join()
 
-    def _await_finalizers():
-        """
-        This function will wait for the completion of all finalizers.
-        """
-        while True:
-            if not new_finalizing.is_set():     # No new running finalizer -> no work
-                with checker_lock:
-                    finished[1] = True
-                    checker_event.set()
-            new_finalizing.wait()       # New finalizer running!
-            with finalization_lock:
-                new_finalizing.clear()
-            with checker_lock:
-                finished[1] = False
-
-            dead_finalizers : Set[DaemonThread] = set()
-            for d in alive_finalizers.copy():   # Joining all running finalizers
-                d.join()
-                dead_finalizers.add(d)
-            alive_finalizers.difference_update(dead_finalizers)     # Clearing them from the running finalizers
-
-    def _check():
-        """
-        This function will wait until the two previous tasks complete.
-        """
-        while True:
-            checker_event.wait()    # I received a checking notification!
-            with checker_lock:
-                if all(finished):   # Both tasks finished -> it is really over now
-                    break
-                checker_event.clear()
-
-    DaemonThread(target = _start_finalizers).start()
-    DaemonThread(target = _await_finalizers).start()
-    d = DaemonThread(target = _check)   # Checker thread to join
-    d.start()
-    d.join()
+    
 
 
 
