78e6a5aca9ded3270ca52703c1336699bb62b4e7
MrVoustache <vincent.raulin.n7@gmail.com>
2023-09-07 21:19:06 +0200

    Forgot that better_threading was no longer a part of Viper...

--- Viper/__init__.py
+++ Viper/__init__.py
@@ -2,4 +2,4 @@
 The Viper library contains many useful tools to improve the capabilities of Python.
 """
 
-__all__ = ["abc", "building", "compress", "debugging", "meta", "better_threading", "exceptions", "format", "frozendict", "interactive", "pickle_utils", "warnings", "io"]
+__all__ = ["abc", "building", "compress", "debugging", "meta", "exceptions", "format", "frozendict", "interactive", "pickle_utils", "warnings", "io"]
