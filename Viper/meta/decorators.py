ebc02605dc00af804f7705ac56a5a35f16455b65
MrVoustache <vincent.raulin.n7@gmail.com>
2023-10-20 14:30:29 +0200

    New version of the meta.decorators module that solves the pickability issue.

--- Viper/meta/decorators.py
+++ Viper/meta/decorators.py
@@ -24,93 +24,18 @@ class Decorator(Generic[P1, R1, P2, R2], metaclass = ABCMeta):
     Just use it as a base class for you decorator classes.
     """
 
-    from pickle import dumps as __dumps
-
-    __existing : WeakValueDictionary[tuple[str, str], "Decorator[P1, R1, P2, R2]"] = WeakValueDictionary()
-
-
-
-    class _GlobalLoader:
-
-        """
-        Internal class used to load global names.
-        """
-
-        from inspect import getmodule
-        __getmodule = staticmethod(getmodule)
-        from importlib import import_module
-        __import_module = staticmethod(import_module)
-        from pickle import PicklingError as __PicklingError
-        del getmodule, import_module
-
-        __slots__ = {
-            "__module" : "The complete name of the module in which the target was defined.",
-            "__name" : "The complete name of the target in the module namespace."
-        }
-
-        def __init__(self, target : Callable[P2, R2]) -> None:
-            module = type(self).__getmodule(target)
-            if not module:
-                raise ModuleNotFoundError(f"Cannot find the module {repr(target)} was defined in")
-            self.__module = module.__name__
-            name = target.__qualname__ if hasattr(target, "__qualname__") else (target.__name__ if hasattr(target, "__name__") else None)
-            if name is None:
-                for k, v in vars(module).items():       # Works at module level, if it was used to make a copy of a function under a different name.
-                    if v is target:
-                        name = k
-                        break
-            if name is None:
-                raise NameError(f"Cannot find {repr(target)} in module '{module.__name__}'")
-            obj = module
-            try:
-                for n in name.split("."):
-                    obj = getattr(obj, n)
-            except (NameError, KeyError, AttributeError):
-                raise NameError(f"Cannot find {repr(target)} in module '{module.__name__}'")
-            if obj is not target:
-                raise type(self).__PicklingError(f"Cannot pickle {repr(target)}: it is not the same as {module.__name__}.{name}")
-            self.__name = name
-            if ":" in name or ":" in module.__name__:
-                raise ValueError(f"Invalid global names function '{name}' in module '{module.__name__}'")
-
-        def load(self) -> Callable[P1, R1]:
-            """
-            Loads the target.
-            """
-            mod = type(self).__import_module(self.__module)
-            obj = mod
-            try:
-                for n in self.__name.split("."):
-                    obj = getattr(obj, n)
-            except (NameError, KeyError, AttributeError):
-                raise NameError(f"Could not load '{self.__name}' from module '{self.__module}'")
-            return obj
-        
-        def __getstate__(self) -> object:       # Just to make the pickle more compact
-            return self.__module + ":" + self.__name
-        
-        def __setstate__(self, state : str):
-            self.__module, self.__name = state.split(":")
-
-
-
-    def __new__(cls, func : Callable[P1, R1]):
-        if not callable(func):
-            raise TypeError(f"Expected callable, got '{type(func).__name__}'")
-        name = func.__qualname__ if hasattr(func, "__qualname__") else func.__name__
-        module = func.__module__ if hasattr(func, "__module__") else None
-        if name and module and (module, name) in cls.__existing:
-            return cls.__existing[module, name]
-        res = super().__new__(cls)
-        if name and module:
-            cls.__existing[module, name] = res
-        return res
+    from pickle import dumps as __dumps, PicklingError as __PicklingError
+    from functools import WRAPPER_ASSIGNMENTS as __WRAPPER_ASSIGNMENTS
+    from importlib import import_module
+    __import_module = staticmethod(import_module)
+    del import_module
 
     def __init__(self, func : Callable[P1, R1]) -> None:
         if not callable(func):
             raise TypeError(f"Expected callable, got '{type(func).__name__}'")
-        if func is not self:
-            self.__wrapped__ = func
+        for name in Decorator.__WRAPPER_ASSIGNMENTS:
+            setattr(self, name, getattr(func, name, None))
+        self.__wrapped__ = func
 
     def __set_name__(self, owner : type, name : str):
         self.__name__ = name
@@ -121,15 +46,48 @@ class Decorator(Generic[P1, R1, P2, R2], metaclass = ABCMeta):
     def __call__(self, *args: P2.args, **kwds: P2.kwargs) -> R2:
         raise NotImplementedError
     
-    def __reduce__(self) -> str | tuple[Any, ...]:
+    def __is_target_pickable(self) -> bool:
+        """
+        Internal function that checks if the wrapped target is itself pickable.
+        """
         try:
             Decorator.__dumps(self.__wrapped__)
-        except:
-            return Decorator._GlobalLoader(self).load, ()
-        try:
-            return Decorator._GlobalLoader(self).load, ()
-        except:
+            return True
+        except Decorator.__PicklingError:
+            return False
+        
+    def __is_global(self) -> bool:
+        """
+        Internal function that checks if the decorator is the global value referenced by target's path.
+        """
+        if self.__module__ is None or self.__qualname__ is None:
+            print("What???")
+            return False
+        mod = Decorator.__import_module(self.__module__)
+        qualname = self.__qualname__
+        obj, qualname = getattr(mod, qualname.partition(".")[0]), qualname.partition(".")[2]
+        while qualname:
+            obj, qualname = getattr(obj, qualname.partition(".")[0]), qualname.partition(".")[2]
+        return obj is self
+    
+    @staticmethod
+    def _global_load(module : str, qualname : str) -> "Decorator":
+        """
+        Internal function used to load a decorated object from a fully qualified module and name.
+        """
+        mod = Decorator.__import_module(module)
+        obj, qualname = getattr(mod, qualname.partition(".")[0]), qualname.partition(".")[2]
+        while qualname:
+            obj, qualname = getattr(obj, qualname.partition(".")[0]), qualname.partition(".")[2]
+        return obj
+    
+    def __reduce__(self) -> str | tuple[Any, ...]:
+        if self.__is_global():
+            return self._global_load, (self.__module__, self.__qualname__)
+        elif self.__is_target_pickable():
             return super().__reduce__()
+        else:
+            raise Decorator.__PicklingError("Unpickable decorated object")
         
 del P1, R1, P2, R2
 
@@ -170,18 +128,14 @@ class semistaticmethod(Decorator[Concatenate[T | None, P], R, P, R]):
             return f"<null-bound method {self.__func.__qualname__} at {address}>"
         
 
-
-    def __init__(self, func : Callable[Concatenate[T | None, P], R]) -> None:
-        super().__init__(func)
-        self.__func = func
     
     def __repr__(self) -> str:
-        address = hex(id(self.__func))[2:].upper()
+        address = hex(id(self.__wrapped__))[2:].upper()
         address = "0x" + ("0" * (16 - len(address))) + address
-        return f"<semistaticmethod {self.__func.__qualname__} at {address}>"
+        return f"<semistaticmethod {self.__wrapped__.__qualname__} at {address}>"
 
     def __call__(self, instance : T | None, *args : P.args, **kwargs : P.kwargs) -> R:
-        return self.__func(instance, *args, **kwargs)
+        return self.__wrapped__(instance, *args, **kwargs)
     
     @overload
     def __get__(self, obj : T , cls : type[T] | None = None) -> Callable[P, R]:
@@ -209,18 +163,14 @@ class hybridmethod(Decorator[Concatenate[T | type[T], P], R, P, R]):
     """
 
     from types import MethodType as __MethodType
-
-    def __init__(self, func : Callable[Concatenate[T | type[T], P], R]) -> None:
-        super().__init__(func)
-        self.__func = func
     
     def __repr__(self) -> str:
-        address = hex(id(self.__func))[2:].upper()
+        address = hex(id(self.__wrapped__))[2:].upper()
         address = "0x" + ("0" * (16 - len(address))) + address
-        return f"<hybridmethod {self.__func.__qualname__} at {address}>"
+        return f"<hybridmethod {self.__wrapped__.__qualname__} at {address}>"
 
     def __call__(self, instance_or_class : T | type[T], *args : P.args, **kwargs : P.kwargs) -> R:
-        return self.__func(instance_or_class, *args, **kwargs)
+        return self.__wrapped__(instance_or_class, *args, **kwargs)
 
     @overload
     def __get__(self, obj : None, cls : type[T] | None = None) -> Callable[P, R]:
@@ -245,6 +195,7 @@ class staticproperty(property, Generic[P, R, T]):
     """
     This decorator transforms a method into a static property of the class (it takes no self/cls argument).
     You can use setter, getter and deleter to set the different staticproperty descriptors.
+    Note that for now, only the getter will work when called from the class.
     """
 
     def __init__(self, fget : Callable[[], R] | None = None, fset : Callable[[R], None] | None = None, fdel : Callable[[], None] | None = None, *args) -> None:
